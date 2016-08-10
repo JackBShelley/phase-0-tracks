029fd441b9757530e9fff622933614ee8ebc2360
Jack Shelley <bearliver@gmail.com>
2016-08-09 20:26:17 -0700

    Progress commit: Added new value logic

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,15 +2,30 @@ puts "What's your name?"
 name = gets.chomp
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
-puts "Do you have children?"
+puts "I have children? (T/F)"
 children = gets.chomp
 
 puts "What theme would you like?"
 theme = gets.chomp
 
-puts "Do you have animals?"
+puts "I you have animals? (T/F)"
 animal = gets.chomp
+children == "t" ? (children = TRUE) : (children = false)
+animal == "t" ? (animal = TRUE) : (animal = false)
 
 design_hash = {:name => name, :age => age, :children => children, :theme => theme, :animal => animal}
+
+puts "Would you like to update any values?"
+input = gets.chomp.to_sym
+
+
+if input == :none
+	p design_hash
+else 
+	puts "What is the new value?"
+  	design_hash[input] = gets.chomp
+	p design_hash
+end
+
