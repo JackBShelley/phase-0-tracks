6de0fd2aac61b9dfd57052a1407255efcc76d69d
Jack Shelley <bearliver@gmail.com>
2016-08-13 16:39:57 -0700

    Capitalization is now working

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,13 +10,20 @@ def fake_name(string)
 	string_array.insert(0, string_array.delete_at(1))
 	string_array = string_array.join(' ')
 
-	string_array = string_array.downcase.tr('bcdfghjklmnpqrstvwxyzaeiou', 'cdfghjklmnpqrstvwxyzbeioua').split(' ')
-	
-	string_array[0].capitalize!
-	string_array[1].capitalize!
-	string_array = string_array.join(' ')
- 
+	string_array = string_array.downcase.tr('bcdfghjklmnpqrstvwxyzaeiou',     'cdfghjklmnpqrstvwxyzbeioua')
+
+	if string_array.include?(' ') == TRUE
+		string_array = string_array.split(' ')
+		
+		x = 0
+		
+		string_array.each do |i|
+		string_array[x].capitalize!
+		x =+ 1
+		end
+		string_array = string_array.join(' ')
+	end
 end
 
 
-fake_name("Jack Shelley")
+fake_name("swag the boy")
