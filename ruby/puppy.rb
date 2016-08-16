e60f5c971b11575b50633cc0f1c31825ae01c545
Jack Shelley <bearliver@gmail.com>
2016-08-15 17:07:42 -0700

    Added characteristics and behavior examples to puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Canis lupus familiaris
+
+# CHARACTERISTICS ----------------
+# Breed: varies
+# Color: varies
+# tail length: varies
+# age: varies
+# size: varies
+
+# BEHAVIOR -----------------------
+# Jump
+# sit
+# roll over
