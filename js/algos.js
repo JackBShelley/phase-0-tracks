cc9659352ebdd333923ac9eb75e8bc9a73e3eb73
Jack Shelley <bearliver@gmail.com>
2016-08-28 18:03:52 -0700

    Working on shared_pair function

--- js/algos.js
+++ js/algos.js
@@ -15,6 +15,27 @@ function longest_word(arr){
 	console.log(longest)
 }
 
+
+
+function shared_pair(obj1, obj2){
+	for (var i = 0; i < 2; i++) {
+		
+		if (obj1.name === obj2.name) {
+			return true;
+		}
+		else if (obj1.age === obj2.age) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
 longest_word(["one", "nine", "hamburger", "toads"]);
 longest_word(["superlongword", "smol", "tiny", "little"]);
 longest_word(["Wow", "Ok"]);
+
+
+shared_pair({name: "Steven", age: 54}, {name: "Tamir", age: 54});
+shared_pair({name: "Steven", age: 54}, {name: "Tamir", age: 53});
