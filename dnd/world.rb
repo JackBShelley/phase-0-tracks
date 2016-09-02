b1a4c9060c229360a9a36772893d5b1ca3d5c073
Jack Shelley <bearliver@gmail.com>
2016-09-02 14:27:38 -0700

    Realm list operational

--- dnd/world.rb
+++ dnd/world.rb
@@ -1,49 +1,20 @@
 require 'sqlite3'
 
-# def world_info
-#   puts "World Info:
-#   (1). Current realms
-#   (2). Add realm
-#   (3). Delete realm
-#   (0). <---Back"
-#   character_input = gets.chomp
-
-#   if character_input == "1"
-#     current_realms
-#   elsif character_input == "2"
-#     puts "add realm"
-#   elsif character_input == "3"
-#     puts delete realm
-#   elsif character_input == "0"
-#    	main_menu
-#   else
-#     puts "Please enter valid number"
-#     monster_index
-#   end
-# end
-
-# def current_realms
-# 	puts "The current world is #{world_name}"
-# 	puts " "
-# end
 
 def world_info
-    puts "Monster index:
-  (1). Monsters by name
-  (2). Monsters by location
-  (3). Add Monster
-  (4). Delete Monster
+    puts "World Info:
+  (1). Current realms
+  (2). Add realm
+  (3). Delete realm
   (0). <---Back"
   character_input = gets.chomp
 
   if character_input == "1"
     current_realms
   elsif character_input == "2"
-    p "monsters by location"
+    p "add realm"
   elsif character_input == "3"
-    p "add monster"
-  elsif character_input == "4"
-    "delete monster"
+    p "delete realm"
   elsif character_input == "0"
     main_menu
   else
@@ -54,7 +25,7 @@ end
 
 def current_realms
   puts " "
-  puts "Enter monster number for information"
+  puts "Enter realm number for information"
   name_hash = $DATABASE.execute("SELECT name FROM realms;")
 
   i = 0
@@ -64,20 +35,20 @@ def current_realms
   end
   puts "(0) <---Back"
   puts ""
-  puts "Enter number for info on monster, or 0 to exit."
-  monster_input = gets.chomp.to_i
-  monster_name = $DATABASE.execute("SELECT name, id FROM monsters WHERE id = #{monster_input}")
+  puts "Enter number for info on realm, or 0 to exit."
+  realm_input = gets.chomp.to_i
+  realm_name = $DATABASE.execute("SELECT name, id FROM realms WHERE id = #{realm_input}")
 
-  if monster_input == 0
+  if realm_input == 0
     world_info
   else
     puts " "
-    puts "#{monster_name[0][0]}"
-    puts "#{$DATABASE.execute("SELECT description FROM monsters WHERE id = #{monster_input}")[0]["description"]}"
+    puts "#{realm_name[0][0]}"
+    puts "#{$DATABASE.execute("SELECT description FROM realms WHERE id = #{realm_input}")[0]["description"]}"
   end
 
   puts " "
-  puts "Press enter to check another monster, 0 to go back"
+  puts "Press enter to check another realm, 0 to go back"
 
   after_input = gets.chomp
 
