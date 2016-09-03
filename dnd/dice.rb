a48248e5fb497c2972af21116e94d6ea332daa90
Jack Shelley <bearliver@gmail.com>
2016-09-02 21:11:11 -0700

    Dice roll history erase works

--- dnd/dice.rb
+++ dnd/dice.rb
@@ -1,6 +1,31 @@
 require 'sqlite3'
 
+
 def roll_dice
+	puts ""
+	puts "Dice Roll:
+(1) Roll
+(2) Roll History
+(3) Erase Roll History
+(0) <---Back
+	"
+	user_input = gets.chomp
+
+	if user_input == "1"
+		roll_dice_action
+	elsif user_input == "2"
+		dice_roll_history
+	elsif user_input == "3"
+		delete_roll_history
+	elsif user_input == "0"
+		main_menu
+	else 
+		puts "Invalid input. Enter 0-2"
+	end
+end
+
+
+def roll_dice_action
 	user_input = nil
 	puts "type 0 at any time to quit"
 	puts " "
@@ -15,28 +40,33 @@ def roll_dice
 		puts "How many sides?"
 		user_input = gets.chomp.to_i
 
-		add_role = <<-SQLITE3
+		add_roll = <<-SQLITE3
          INSERT INTO dice_rolls (value) VALUES (#{roll});
         SQLITE3
-        $DATABASE.execute(add_role)
-
+        $DATABASE.execute(add_roll)
 		end
 	end
 
 	roll_data = $DATABASE.execute("SELECT value FROM dice_rolls")
-
-	if roll_data.length > 10
-		delete_roll = <<-SQLITE3
-         DELETE FROM dice_rolls WHERE id>10
-         SQLITE3
-        $DATABASE.execute(delete_roll)
-        main_menu
-    else 
-    	main_menu
-    end
+	roll_dice
 
 
 end
 
+def dice_roll_history
+	roll_history = $DATABASE.execute("SELECT * from dice_rolls order by id desc limit 10;")
+	p " "
+	puts "Roll History (Most recent to least)"
+    roll_history.each do |roll|
+    	puts "#{roll["value"]}"
+    end
+    roll_dice
+end
+
+def delete_roll_history
+	$DATABASE.execute("DELETE FROM dice_rolls;")
+	p "Roll history deleted"
+	roll_dice
+end
 
 # rolls = $DATABASE.execute("SELECT value FROM dice_roll;")
